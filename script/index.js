29a71c6483e0464586e156530f7f9c985e473216
Felipe Santana <felipesantannna511@gmail.com>
2026-03-06 10:54:46 -0300

    projeto2

--- script/index.js
+++ script/index.js
@@ -1,5 +1,9 @@
 document.getElementById("projeto-1").addEventListener("click", function() {
     window.location.href = "projeto/calculadora-simples-main/calculadora.html";
 });
+
+document.getElementById("projeto-2").addEventListener("click", function(){
+    window.location.href = "projeto/site/index.html";
+});
  
 
