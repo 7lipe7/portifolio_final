b51b78a894820a8290897627c956173513cbff70
Felipe Santana <felipesantannna511@gmail.com>
2026-03-06 21:15:22 -0300

    final

--- script/index.js
+++ script/index.js
@@ -3,7 +3,10 @@ document.getElementById("projeto-1").addEventListener("click", function() {
 });
 
 document.getElementById("projeto-2").addEventListener("click", function(){
-    window.location.href = "projeto/site/index.html";
+    window.location.href = "/projeto/hdc/index.html";
+});
+document.getElementById("projeto-3").addEventListener("click", function(){
+    window.location.href = "/projeto/blog/index.html";
 });
  
 
